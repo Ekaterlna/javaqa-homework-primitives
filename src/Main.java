e8f4811a94c237f3526e5850290b96f24ba02aba
Katest <103419049+Ekaterlna@users.noreply.github.com>
2023-06-20 16:54:26 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_600;
+        int bonusMileRub = 20;
+        int bonusTicket = ticketPrice / bonusMileRub;
+        System.out.println(bonusTicket);
+    }
+}
